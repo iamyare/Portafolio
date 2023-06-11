8da704dac6362029cc808f883864ceb9cfccdc26
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2023-06-11 14:15:33 -0600

    Correciones

---
+++ assets/js/controlador.js
@@ -0,0 +1,4 @@
+//Pantalla de carga, verificar si el navegador a cargado la pagina y si es asi, ocultar la pantalla de carga #pantallaCarga
+window.onload = function () {
+	document.getElementById("pantallaCarga").classList.add("desactive");
+};
